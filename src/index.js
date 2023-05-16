6b5bcd62b8b577d4840608758dcd13241bfe1779
Cesar Verissimo <cesarjunior@unigranrio.br>
2023-05-16 12:02:57 -0300

    Adicionado trecho de Axios do MongoDB

--- src/index.js
+++ src/index.js
@@ -20,3 +20,32 @@ mongoose.connect('mongodb://localhost:27017/PCA-2023.1-Biblioteca', {useNewUrlPa
 }).catch((error) => {
  console.log(error)
 });*/
+
+var axios = require('axios');
+var data = JSON.stringify({
+    "collection": "Usuario",
+    "database": "databasedeteste",
+    "dataSource": "Clusterdeteste",
+    "projection": {
+        "_id": 1
+    }
+});
+            
+var config = {
+    method: 'post',
+    url: 'https://sa-east-1.aws.data.mongodb-api.com/app/data-hqpuo/endpoint/data/v1/action/findOne',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': '*',
+      'api-key': 'a3DmEFsA8qSIv9s7HlrXzG6q254IRSapfqgxXhwFbWQQTZy5Sl9W19F2ahpEFJSA',
+    },
+    data: data
+};
+            
+axios(config)
+    .then(function (response) {
+        console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
