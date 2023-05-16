efe52dd782ce42aeed8480aa9a7c351191886c49
Cesar Verissimo <cesarjunior@unigranrio.br>
2023-05-16 11:55:08 -0300

    Possivel criação auto de key para usuario

--- src/api.js
+++ src/api.js
@@ -11,3 +11,16 @@ exports.add = function (req, res) {
   exports.delete = function (req, res) {
     res.send({type: 'DELETE'});
   };
+
+import React, {useEffect} from 'react';
+import {useUser} from '@realm/react';
+function UserApiKeys() {
+  const user = useUser();
+  async function createUserApiKey() {
+    const apiKey = await user?.apiKeys.create('mySecretKey');
+    // ...Do something with API key like save it
+    // or share it with external service that authenticates
+    // on user's behalf.
+  }
+  // ...
+}
